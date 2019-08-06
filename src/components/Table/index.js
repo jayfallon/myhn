47a55738542fa44702fdc53924485c8ba89d10fb
Jay Fallon <jayfallon@gmail.com>
2019-08-06 19:35:13 -0400

    refactored code org

---
+++ src/components/Table/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Button from '../Button';
+
+class Table extends Component {
+  render() {
+    const { list, onDismiss } = this.props;
+    return (
+      <div className="list__display">
+        {list.map(item => (
+          <ul key={item.objectID}>
+            <li>
+              <a href={item.url}>{item.title}</a>
+            </li>
+            <li>{item.author}</li>
+            <li>{item.num_comments}</li>
+            <li>{item.points}</li>
+            <li>
+              <Button
+                className={item.title}
+                onClick={() => onDismiss(item.objectID)}
+              >
+                Dismiss
+              </Button>
+            </li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Table;
